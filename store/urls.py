29f1b725604bfc43f70acbaaeda114e3f4013bf1
dattt-cy <victomblack1602@gmail.com>
2025-09-30 19:16:14 +0700

    "add greatKart and Orders"

--- store/urls.py
+++ store/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for beeshop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 from . import views
 
@@ -22,5 +5,6 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
-    path('search/', views.search, name='search')
+    path('search/', views.search, name='search'),
+    path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
 ]
